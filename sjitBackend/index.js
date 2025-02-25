d316866a9372200d44c8d01c6a3998813ea90f34
Krithika <dhkrithika1114@gmail.com>
2025-02-25 13:05:51 +0530

    DAY 13 FN

--- sjitBackend/index.js
+++ sjitBackend/index.js
@@ -1,15 +1,51 @@
-const express = require('express')
+const express = require("express");
+const mdb = require("mongoose");
+const dotenv=require('dotenv')
+const Signup=require('./models/signupSchema.js')
+const app = express();
+app.use(express.json)
+const PORT = 3001;
+dotenv.config()
 
-const app = express()
+mdb
+  .connect("process.env.MONGODB_URL") //if it doesn't connect with localhost, replace it with 127.0.0.1 ip
+  .then(() => {
+    console.log("MDB Connection Successful");
+  })
+  .catch((err) => {
+    console.log("Check your connection String", err);
+  });
+  
 
-app.get("/",(req,res)=>{
-    res.send("<h1>Welcome to Backend</h1>")
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to Backend</h1>");
+});
 
-app.get("/static",(req,res)=>{
-    res.sendFile("D:\\SJITMERN2025\\DAY 3\\index.html")
-})
+app.get("/static", (req, res) => {
+  res.sendFile("D:\\SJITMERN2025\\DAY 3\\index.html");
+});
 
-const PORT =3001
+app.post("/signup",(req,res)=>{
+    try{
+        console.log(req.body);
+        const{firstName,lastName,email,password,phoneNumber}=req.body;
+        const newSignup=new Signup({
+            firstName:firstName,
+            lastName:lastName,
+            email:email,
+            password:password,
+            phoneNumber:phoneNumber,
+        });
+        newSignup.save();
+        console.log("Signup Successful");
+        ({message:"Signup Successful",isSignUp:true});
+    }catch(error){
+        console.log(error);
+        res.status(400).json({message:"Signup Unsuccessful",isSignUp:false});
+    }
+});
 
-app.listen(PORT,()=>console.log("Server Started Successfully"))
+
+app.listen(PORT, () => {
+    console.log("Server Started Successfully")
+});
