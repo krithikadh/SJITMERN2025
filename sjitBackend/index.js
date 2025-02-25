11bead7667788982f9d99830b1f498edd780d0c6
Krithika <dhkrithika1114@gmail.com>
2025-02-25 14:45:05 +0530

    DAY 13 AN

--- sjitBackend/index.js
+++ sjitBackend/index.js
@@ -1,51 +1,51 @@
 const express = require("express");
 const mdb = require("mongoose");
-const dotenv=require('dotenv')
-const Signup=require('./models/signupSchema.js')
+const dotenv = require("dotenv");
+const bcrypt = require("bcrypt");
+const Signup = require("./models/signupSchema");
 const app = express();
-app.use(express.json)
+app.use(express.json());
 const PORT = 3001;
-dotenv.config()
+dotenv.config();
 
 mdb
-  .connect(process.env.MONGODB_URL) //if it doesn't connect with localhost, replace it with 127.0.0.1 ip
+  .connect(process.env.MONGODB_URL) // if it doesn't connect with localhost replace it with 127.0.0.1 ip address
   .then(() => {
     console.log("MDB Connection Successful");
   })
   .catch((err) => {
     console.log("Check your connection String", err);
   });
-  
 
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to Backend</h1>");
 });
-
 app.get("/static", (req, res) => {
-  res.sendFile("D:\\SJITMERN2025\\DAY 3\\index.html");
+  res.sendFile(
+    "/Users/prasanthksp/Documents/RAMPeX-Parent-Folder/Trainings/SJIT2025MERN/HTML_CSS/index.html"
+  );
 });
 
-app.post("/signup",(req,res)=>{
-    try{
-        console.log(req.body);
-        const{firstName,lastName,email,password,phoneNumber}=req.body;
-        const newSignup=new Signup({
-            firstName:firstName,
-            lastName:lastName,
-            email:email,
-            password:password,
-            phoneNumber:phoneNumber,
-        });
-        newSignup.save();
-        console.log("Signup Successful");
-        res.status(201).json({message:"Signup Successful",isSignUp:true});
-    }catch(error){
-        console.log(error);
-        res.status(400).json({message:"Signup Unsuccessful",isSignUp:false});
-    }
+app.post("/signup", async (req, res) => {
+  try {
+    console.log(req.body);
+    const { firstName, lastName, email, password, phoneNumber } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newSignup = new Signup({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: hashedPassword,
+      phoneNumber: phoneNumber,
+    });
+    newSignup.save();
+    console.log("Signup Successful");
+    res.status(201).json({ message: "Signup Successful", isSignUp: true });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: "Signup Unsuccessful", isSignUp: false });
+  }
 });
-
-
 app.listen(PORT, () => {
-    console.log("Server Started Successfully")
+  console.log("Server Started Successfully");
 });
